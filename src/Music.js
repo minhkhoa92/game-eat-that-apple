67877b074ba7c21a7098a8b7dcd2b37953331df3
Minh Ngo <mkngo92@gmail.com>
2026-03-15 21:55:28 +0100

    lowering volume of music

--- src/Music.js
+++ src/Music.js
@@ -2,7 +2,7 @@ import morningHasBroken from './music/morning_has_broken_reprise.wav';
 
 function startMusic() {
   let mySound = new Audio(morningHasBroken);
-  mySound.volume = 0.05;
+  mySound.volume = 0.03;
   mySound.play();
 }
 export {startMusic};
